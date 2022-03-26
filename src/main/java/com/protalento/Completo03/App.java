dfcfa69f1983d57f40b6c6e9b6483d538388661c
santiagobp24 <santiago83iti@gmail.com>
2022-03-26 14:17:56 -0500

    primer master

---
+++ src/main/java/com/protalento/Completo03/App.java
@@ -0,0 +1,13 @@
+package com.protalento.Completo03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( 1+2 ) ;
+    }
+}
