6d818eedbaf291582d7589f113f1a35287ad2c15
santiagobp24 <santiago83iti@gmail.com>
2022-03-26 14:32:15 -0500

    segundo feature1

--- src/main/java/com/protalento/Completo03/App.java
+++ src/main/java/com/protalento/Completo03/App.java
@@ -14,6 +14,6 @@ public class App
         
         System.out.println( 1+2 ); // 1 develop
         
-        System.out.println( 1+2 ); // 1 feature1
+        System.out.println( 1+2 ); // 1 feature1 2 feature1
     }
 }
