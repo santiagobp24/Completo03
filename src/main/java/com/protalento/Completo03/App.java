8bbd01cf7a30f34bcddcade06db12f1c77f0b58a
santiagobp24 <santiago83iti@gmail.com>
2022-03-26 16:36:33 -0500

    tercer master

--- src/main/java/com/protalento/Completo03/App.java
+++ src/main/java/com/protalento/Completo03/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( 1+2 )// 1 master ;
+        System.out.println( 1+2 )// 1 master 2 master
         
-        System.out.println( 1+2 ) // 1 pre-prod;
+        System.out.println( 1+2 ); // 1 pre-prod
     }
 }
