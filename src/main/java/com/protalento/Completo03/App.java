8f496a4fa81798fae311f619a18d1c643d2e9691
santiagobp24 <santiago83iti@gmail.com>
2022-03-26 17:02:24 -0500

    cuarto master

--- src/main/java/com/protalento/Completo03/App.java
+++ src/main/java/com/protalento/Completo03/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( 1+2 )// 1 master 2 master
+        System.out.println( 1+2 )// 1 master 2 master 3 master
         
         System.out.println( 1+2 ); // 1 pre-prod 2 pre-prod 3 preprod
         
